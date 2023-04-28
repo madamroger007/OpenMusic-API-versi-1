c3e9ae765f6a32f0f51a02cba39e9bb187bfe642
madam007 <adamsetiadijr07@gmail.com>
2023-04-28 17:49:52 +0700

    first commit

---
+++ src/api/songs/handler.js
@@ -0,0 +1,77 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-console */
+/* eslint-disable no-underscore-dangle */
+const autoBind = require('auto-bind');
+
+class SongsHandler {
+  constructor(service, validator) {
+    this._service = service;
+    this._validator = validator;
+
+    autoBind(this); // mem-bind nilai this untuk seluruh method sekaligus
+  }
+
+  async postSongHandler(request, h) {
+    this._validator.validateSongPayload(request.payload);
+    const {
+      title, year, genre, performer, duration, albumId,
+    } = request.payload;
+    const songId = await this._service.addSong({
+      title, year, genre, performer, duration, albumId,
+    });
+    const response = h.response({
+      status: 'success',
+      // message: 'Lagu berhasil ditambahkan',
+      data: {
+        songId,
+      },
+    });
+
+    response.code(201);
+    return response;
+  }
+
+  async getSongHandler() {
+    const songs = await this._service.getSongs();
+    return {
+      status: 'success',
+      data: {
+        songs,
+      },
+    };
+  }
+
+  async getSongByIdHandler(request) {
+    const { id } = request.params;
+    const song = await this._service.getSongById(id);
+    return {
+      status: 'success',
+      data: {
+        song,
+      },
+    };
+  }
+
+  async putSongByIdHandler(request) {
+    this._validator.validateSongPayload(request.payload);
+    const { id } = request.params;
+    await this._service.editSongById(id, request.payload);
+
+    return {
+      status: 'success',
+      message: 'Lagu berhasil diperbarui',
+    };
+  }
+
+  async deleteSongByIdHandler(request) {
+    const { id } = request.params;
+    await this._service.deleteSongById(id);
+
+    return {
+      status: 'success',
+      message: 'Lagu berhasil dihapus',
+    };
+  }
+}
+
+module.exports = SongsHandler;
---
+++ src/server.js
@@ -0,0 +1,75 @@
+/* eslint-disable new-cap */
+require('dotenv').config();
+
+const Hapi = require('@hapi/hapi');
+const albums = require('./api/albums');
+const songs = require('./api/songs');
+const albumService = require('./services/postgres/albumService');
+const songService = require('./services/postgres/songService');
+const AlbumsValidator = require('./validator/albums');
+const SongsValidator = require('./validator/songs');
+const ClientError = require('./exceptions/ClientError');
+
+const init = async () => {
+  const albumServices = new albumService();
+  const songServices = new songService();
+  const server = Hapi.server({
+    port: process.env.PORT,
+    host: process.env.HOST,
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+
+  await server.register({
+    plugin: albums,
+    options: {
+      service: albumServices,
+      validator: AlbumsValidator,
+    },
+  });
+
+  await server.register({
+    plugin: songs,
+    options: {
+      service: songServices,
+      validator: SongsValidator,
+    },
+  });
+
+  server.ext('onPreResponse', (request, h) => {
+    // mendapatkan konteks response dari request
+    const { response } = request;
+    if (response instanceof Error) {
+      // penanganan client error secara internal.
+      if (response instanceof ClientError) {
+        const newResponse = h.response({
+          status: 'fail',
+          message: response.message,
+        });
+        newResponse.code(response.statusCode);
+        return newResponse;
+      }
+      // mempertahankan penanganan client error oleh hapi secara native, seperti 404, etc.
+      if (!response.isServer) {
+        return h.continue;
+      }
+      // penanganan server error sesuai kebutuhan
+      const newResponse = h.response({
+        status: 'error',
+        message: 'terjadi kegagalan pada server kami',
+      });
+      newResponse.code(500);
+      return newResponse;
+    }
+    // jika bukan error, lanjutkan dengan response sebelumnya (tanpa terintervensi)
+    return h.continue;
+  });
+
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
